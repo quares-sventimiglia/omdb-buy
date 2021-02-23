5a521d9f0a604bd8462959f1262b6b500710517d
seba-ventimiglia <sventimiglia@quaresitsolutions.com>
2021-02-23 08:30:08 -0300

    add some details and readme

--- README.md
+++ README.md
@@ -2,12 +2,15 @@ This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next
 
 ## Getting Started
 
-First, run the development server:
+First, clone this project and in the main directory:
 
-```bash
+```
+npm i
+```
+Then, execute:
+
+```
 npm run dev
-# or
-yarn dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
@@ -30,5 +33,7 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 ## Deploy on Vercel
 
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
+
+This aplication is deploy on [Vercel](https://omdb-buy.vercel.app/)
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
--- pages/api/checkout.js
+++ pages/api/checkout.js
@@ -1,28 +1,41 @@
 import mercadopago from "mercadopago";
 
 export default async function (req, res) {
-
-	const {title, year} = req.body
+  const { title, year, plot, image } = req.body;
 
   mercadopago.configure({
     access_token:
       "TEST-7835178460898996-022216-012217c509161746d42930f376bf1b2c-709573187",
   });
 
-  let preference = {
+  const preference = {
     items: [
       {
+        id: 1234,
+        description: plot,
         title: title,
         unit_price: Number(year),
         quantity: 1,
+        picture_url: image,
       },
     ],
+    payer: {
+      name: "Lucas",
+      surname: "Lacquaniti",
+      email: "lacquatnovic@novic.com",
+    },
+    back_urls: {
+      success: "https://www.tu-sitio/success",
+      failure: "http://www.tu-sitio/failure",
+      pending: "http://www.tu-sitio/pending",
+    },
+    auto_return: "approved",
   };
 
   mercadopago.preferences
     .create(preference)
     .then((response) => {
-			res.status(200).json(response.body)
+      res.status(200).json(response.body);
     })
     .catch((error) => {
       console.log(error);
